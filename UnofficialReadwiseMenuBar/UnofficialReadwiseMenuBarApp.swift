1691fe8e9fdb01f955ad86e91196cf0f92ed78f6
TwizzyIndy <twizzy.indy@gmail.com>
2022-06-23 17:56:43 +0630

    init statusBarItem on app startup

--- UnofficialReadwiseMenuBar/UnofficialReadwiseMenuBarApp.swift
+++ UnofficialReadwiseMenuBar/UnofficialReadwiseMenuBarApp.swift
@@ -10,11 +10,30 @@ import SwiftUI
 @main
 struct UnofficialReadwiseMenuBarApp: App {
     let persistenceController = PersistenceController.shared
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    static private(set) var instance: AppDelegate!
+    
+    // The NSStatusBar manages a collection of status items displayed within a system-wide menu bar.
+    lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    let menu = MainMenu()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        AppDelegate.instance = self
+        
+        statusBarItem.button?.image = NSImage(systemSymbolName: "star.fill", accessibilityDescription: nil)
+        statusBarItem.button?.imagePosition = .imageLeading
+        statusBarItem.menu = menu.build()
     }
 }
