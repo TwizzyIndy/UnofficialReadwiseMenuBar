6c7119cddcb51afc6c6abea61664e5cfa00c5ed3
TwizzyIndy <twizzy.indy@gmail.com>
2022-06-26 14:20:13 +0630

    StorageKeys added

---
+++ UnofficialReadwiseMenuBar/Models/StorageKeys.swift
@@ -0,0 +1,13 @@
+//
+//  StorageKeys.swift
+//  UnofficialReadwiseMenuBar
+//
+//  Created by Aung Khant M. on 26/06/2022.
+//
+
+import Foundation
+
+enum StorageKeys: String {
+    case api_key
+    case key_validated
+}
