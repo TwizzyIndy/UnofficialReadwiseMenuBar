8a0fa770927d207f59132506f96797a86263c17f
TwizzyIndy <twizzy.indy@gmail.com>
2022-06-27 14:49:58 +0630

    wrapped with ScrollView

--- UnofficialReadwiseMenuBar/Views/HighlightView.swift
+++ UnofficialReadwiseMenuBar/Views/HighlightView.swift
@@ -26,42 +26,43 @@ struct HighlightView: View {
         RadialGradient(gradient: Gradient(colors: [.orange, .yellow]), center: .topLeading, startRadius: 100, endRadius: 200)
             .ignoresSafeArea()
             .overlay(
-        Image("highlight_transparent_background")
-            .overlay(
-        VStack {
-            Text(highlight_text)
-                .italic()
-                .font(.custom("Georgia", size: 14.0))
-                .padding()
-                .multilineTextAlignment(.center)
-
-            
-            HStack {
-                Text(author)
-                    .bold()
-                
-                Text("—— from " + bookTitle)
-            }
-            
-            Button("Refresh", action: {
-                highlight_text = self.highlightVM.highlighted_text
-                author = self.highlightVM.author_name
-                bookTitle = self.highlightVM.book_title
-            })
-        }
-        .onAppear()
-        .task {
-            await self.highlightVM.fetchHighlightList(token: appStorageAPIKey)
-            await self.highlightVM.fetchBooksList(token: appStorageAPIKey)
-            
-            highlight_text = self.highlightVM.highlighted_text
-            author = self.highlightVM.author_name
-            bookTitle = self.highlightVM.book_title
-        }
-        .frame(width: 400, height: 200, alignment: .center)
-        .cornerRadius(12.0)
-        )
-        )
+                Image("highlight_transparent_background")
+                    .overlay(
+                        VStack {
+                            ScrollView(.vertical, showsIndicators: false) {
+                                Text(highlight_text)
+                                    .italic()
+                                    .font(.custom("Georgia", size: 14.0))
+                                    .padding()
+                                    .multilineTextAlignment(.center)
+                            }
+                            
+                            HStack {
+                                Text(author)
+                                    .bold()
+                                
+                                Text("—— from " + bookTitle)
+                            }
+                            
+                            Button("Refresh", action: {
+                                highlight_text = self.highlightVM.highlighted_text
+                                author = self.highlightVM.author_name
+                                bookTitle = self.highlightVM.book_title
+                            })
+                        }
+                            .onAppear()
+                            .task {
+                                await self.highlightVM.fetchHighlightList(token: appStorageAPIKey)
+                                await self.highlightVM.fetchBooksList(token: appStorageAPIKey)
+                                
+                                highlight_text = self.highlightVM.highlighted_text
+                                author = self.highlightVM.author_name
+                                bookTitle = self.highlightVM.book_title
+                            }
+                            .frame(width: 400, height: 200, alignment: .center)
+                            .cornerRadius(12.0)
+                    )
+            )
     }
     
     
