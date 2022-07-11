4ff6649489340509ac22f55de631d9ec84dbe2e9
TwizzyIndy <twizzy.indy@gmail.com>
2022-07-11 14:38:57 +0630

    fetch all lists from API implemented

--- UnofficialReadwiseMenuBar/ViewModels/HighlightViewVM.swift
+++ UnofficialReadwiseMenuBar/ViewModels/HighlightViewVM.swift
@@ -12,8 +12,8 @@ class HighlightViewVM: ObservableObject {
     
     //MARK: - Published Objects
     @Published private var auth_status_code: Int?
-    @Published private var hightlight_list: HighlightListModel?
-    @Published private var books_list: BooksListModel?
+    @Published private var highlight_list_array: [HighlightListModel]?
+    @Published private var books_list_array: [BooksListModel]?
     
     //MARK: - For Core Data Handling
     var viewContext : NSManagedObjectContext?
@@ -21,7 +21,7 @@ class HighlightViewVM: ObservableObject {
     var currentHighlightItem : HighlightItemModel?
     
     var highlighted_text: String {
-        guard let item = hightlight_list?.results.randomElement() else {
+        guard let item = highlight_list_array?.randomElement()?.results.randomElement() else {
             return "N/A"
         }
         
@@ -74,197 +74,198 @@ class HighlightViewVM: ObservableObject {
         }
     }
     
-    func getHighlightList(token: String, completion: @escaping() -> Void)
-    {
-        ReadwiseAPI().getHighlightsList(token: token) { result in
-            switch result {
-            case .success(let p):
-                DispatchQueue.main.async {
-                    print("\(#function) ok")
-                    self.hightlight_list = p
-                    completion()
-                }
-            case .failure(_ ):
-                print("error")
-            }
-        }
-    }
-    
-    func getBooksList(token: String, completion: @escaping() -> Void)
-    {
-        ReadwiseAPI().getBooksList(token: token) { result in
-            switch result {
-            case .success(let models):
-                DispatchQueue.main.async {
-                    print("\(#function) ok")
-                    self.books_list = models
-                    completion()
-                }
-            case .failure(_ ):
-                print("\(#function) error")
-            }
-        }
-    }
     
     func fetchHighlightList(token: String) async {
         do {
-            let highlightList = try await ReadwiseAPI().getHighlightsList(token: token)
+            let highlightArrayList = try await ReadwiseAPI().getHighlightsListAll(token: token)
+            
             DispatchQueue.main.async {
-                self.hightlight_list = highlightList
+                self.highlight_list_array = highlightArrayList
                 print("\(#function) ok")
-                
+
+                // save to db
                 self.saveHighlightListToDB()
             }
-        } catch {
-            print("\(#function) error")
+        } catch ResponseError.BadURL {
+            print("\(#function) bad url")
+        } catch let error as NSError {
+            print("\(#function) \(error.localizedDescription)")
+        }
+    }
+    
+    func fetchBooksList(token: String) async {
+        do {
+            let booksListArray = try await ReadwiseAPI().getBooksListAll(token: token)
+            
+            DispatchQueue.main.async {
+                self.books_list_array = booksListArray
+                print("\(#function) ok")
+                
+                // save to db
+                self.saveBooksListToDB()
+            }
+        } catch ResponseError.BadURL {
+            print("\(#function) bad url")
+
+        } catch let error as NSError {
+            print("\(#function) \(error.localizedDescription)")
+
         }
     }
     
     private func saveHighlightListToDB()
     {
         // save to CoreData
-        guard let viewContext = self.viewContext,
-        let hightlight_list = self.hightlight_list else {
+        guard let viewContext = viewContext,
+        let highlight_list_array = self.highlight_list_array else {
             return
         }
         
-        hightlight_list.results.forEach({ fetchedItem in
+        highlight_list_array.forEach({ highlight_list in
             
-            // Make FetchRequest from CoreData
-            let request: NSFetchRequest<HighlightItemDataModel> = HighlightItemDataModel.fetchRequest()
-            
-            // only matched result with fetchedItem's id
-            request.predicate = NSPredicate(format: "id == %lld", fetchedItem.id)
-            
-            do {
-                let storedItems = try viewContext.fetch(request)
+            highlight_list.results.forEach({ fetchedItem in
                 
-                // if already exists
-                if let storeItem = storedItems.first {
-                    //TODO: .. do something here
-                    print("already exists")
-                    return
-                } else { // if not exists
-                    let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "HighlightItemDataModel", into: viewContext) as! HighlightItemDataModel
-                    itemToSave.id = fetchedItem.id
-                    itemToSave.book_id = fetchedItem.book_id ?? 0
-                    itemToSave.highlighted_at = fetchedItem.highlighted_at ?? ""
-                    itemToSave.updated_at = fetchedItem.updated ?? ""
-                    itemToSave.note = fetchedItem.note
-                    itemToSave.tags = fetchedItem.tags as NSObject?
-                    itemToSave.color = fetchedItem.color ?? ""
-                    itemToSave.text = fetchedItem.text
-                    itemToSave.location = Int64(fetchedItem.location)
-                    itemToSave.location_type = fetchedItem.location_type
-                    itemToSave.url = fetchedItem.url ?? ""
+                // Make FetchRequest from CoreData
+                let request: NSFetchRequest<HighlightItemDataModel> = HighlightItemDataModel.fetchRequest()
+                
+                // only matched result with fetchedItem's id
+                request.predicate = NSPredicate(format: "id == %lld", fetchedItem.id)
+                
+                do {
+                    let storedItems = try viewContext.fetch(request)
                     
+                    // if already exists
+                    if let storeItem = storedItems.first {
+                        //TODO: .. do something here
+                        print("already exists")
+                        return
+                    } else { // if not exists
+                        let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "HighlightItemDataModel", into: viewContext) as! HighlightItemDataModel
+                        itemToSave.id = fetchedItem.id
+                        itemToSave.book_id = fetchedItem.book_id ?? 0
+                        itemToSave.highlighted_at = fetchedItem.highlighted_at ?? ""
+                        itemToSave.updated_at = fetchedItem.updated ?? ""
+                        itemToSave.tags = fetchedItem.tags as? [NSObject]
+                        itemToSave.note = fetchedItem.note
+                        itemToSave.color = fetchedItem.color ?? ""
+                        itemToSave.text = fetchedItem.text
+                        itemToSave.location = Int64(fetchedItem.location)
+                        itemToSave.location_type = fetchedItem.location_type
+                        itemToSave.url = fetchedItem.url ?? ""
+                        
+                    }
+                } catch let error as NSError {
+                    print("failed to fetch highlight items. \(error.localizedDescription)")
                 }
-            } catch {
-                print("failed to fetch highlight items")
-            }
+            })
         })
         
         do {
             try viewContext.save()
-        } catch {
-            print("failed to save highlight items to db")
+        } catch let error as NSError {
+            print("failed to save highlight items to db. \(error.localizedDescription)")
         }
+        
     }
-    
-    func fetchBooksList(token: String) async {
-        do {
-            let booksList = try await ReadwiseAPI().getBooksList(token: token)
-            DispatchQueue.main.async {
-                self.books_list = booksList
-                print("\(#function) ok")
-                
-                self.saveBooksListToDB()
-            }
-        } catch {
-            print("\(#function) error")
-        }
-    }
-    
-    func saveBooksListToDB() {
+
+    private func saveBooksListToDB() {
         // save to CoreData
         guard let viewContext = self.viewContext,
-        let books_list = self.books_list else {
+        let books_list_array = self.books_list_array else {
             return
         }
         
-        books_list.results.forEach({ fetchedItem in
-            // Make FetchRequest from CoreData
-            let request: NSFetchRequest<BookItemDataModel> = BookItemDataModel.fetchRequest()
-            
-            // only matched result with fetchedItem's id
-            request.predicate = NSPredicate(format: "id == %lld", fetchedItem.id)
-            
-            do {
-                let storedItems = try viewContext.fetch(request)
+        books_list_array.forEach({ books_list in
+            books_list.results.forEach({ fetchedItem in
+                // Make FetchRequest from CoreData
+                let request: NSFetchRequest<BookItemDataModel> = BookItemDataModel.fetchRequest()
                 
-                // if already exists
-                if let storeItem = storedItems.first {
-                    //TODO: .. do something here
-                    print("already exists")
-                    return
-                } else { // if not exists
+                // only matched result with fetchedItem's id
+                request.predicate = NSPredicate(format: "id == %lld", fetchedItem.id)
+                
+                do {
+                    let storedItems = try viewContext.fetch(request)
                     
-                    let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "BookItemDataModel", into: viewContext) as! BookItemDataModel
-                    
-                    itemToSave.id = fetchedItem.id
-                    itemToSave.updated_at = fetchedItem.updated ?? ""
-                    itemToSave.tags = fetchedItem.tags as NSObject?
-                    itemToSave.asin = fetchedItem.asin ?? ""
-                    itemToSave.author = fetchedItem.author ?? ""
-                    itemToSave.category = fetchedItem.category ?? ""
-                    itemToSave.cover_image_url = fetchedItem.cover_image_url ?? ""
-                    itemToSave.highlights_url = fetchedItem.highlights_url ?? ""
-                    itemToSave.num_highlights = Int32(fetchedItem.num_highlights ?? 0)
-                    itemToSave.source = fetchedItem.source ?? ""
-                    itemToSave.source_url = fetchedItem.source_url ?? ""
-                    itemToSave.last_highlight_at = fetchedItem.last_highlight_at ?? ""
-                    itemToSave.title = fetchedItem.title ?? ""
-                    
+                    // if already exists
+                    if let storeItem = storedItems.first {
+                        //TODO: .. do something here
+                        print("already exists")
+                        return
+                    } else { // if not exists
+                        
+                        let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "BookItemDataModel", into: viewContext) as! BookItemDataModel
+                        
+                        itemToSave.id = fetchedItem.id
+                        itemToSave.updated_at = fetchedItem.updated ?? ""
+                        itemToSave.asin = fetchedItem.asin ?? ""
+                        itemToSave.tags = fetchedItem.tags as? [NSObject]
+                        itemToSave.author = fetchedItem.author ?? ""
+                        itemToSave.category = fetchedItem.category ?? ""
+                        itemToSave.cover_image_url = fetchedItem.cover_image_url ?? ""
+                        itemToSave.highlights_url = fetchedItem.highlights_url ?? ""
+                        itemToSave.num_highlights = Int32(fetchedItem.num_highlights ?? 0)
+                        itemToSave.source = fetchedItem.source ?? ""
+                        itemToSave.source_url = fetchedItem.source_url ?? ""
+                        itemToSave.last_highlight_at = fetchedItem.last_highlight_at ?? ""
+                        itemToSave.title = fetchedItem.title ?? ""
+                        
+                    }
+                } catch let error as NSError {
+                    print("failed to fetch book list items.  \(error.localizedDescription)")
                 }
-            } catch {
-                print("failed to fetch book list items")
-            }
+            })
         })
         
         do {
             try viewContext.save()
-        } catch {
-            print("failed to save highlight items to db")
+        } catch let error as NSError {
+            print("failed to save book list items to db. \(error.localizedDescription)")
         }
     }
     
-    func getBookAuthor(bookId: Int64) -> String
+    private func getBookAuthor(bookId: Int64) -> String
     {
-        guard let books_list = books_list else {
+        guard let books_list_array = books_list_array else {
             return "N/A"
         }
         
-        let filtered = books_list.results.filter { $0.id == bookId }
-        if (filtered.isEmpty)
-        {
-            return "N/A"
-        } else {
-            return filtered.first?.author ?? "N/A"
-        }
+        var result = "N/A"
+        
+        books_list_array.forEach({ books_list in
+            
+            let filtered = books_list.results.filter { $0.id == bookId }
+            
+            if (filtered.isEmpty)
+            {
+                result = "N/A"
+            } else {
+                result = filtered.first?.author ?? "N/A"
+            }
+        })
+        return result
     }
     
-    func getBookTitle(bookId: Int64) -> String
+    private func getBookTitle(bookId: Int64) -> String
     {
-        guard let books_list = books_list else {
+        guard let books_list_array = books_list_array else {
             return "N/A"
         }
         
-        let filtered = books_list.results.filter { $0.id == bookId }
-        if (filtered.isEmpty)
-        {
-            return "N/A"
-        } else {
-            return filtered.first?.title ?? "N/A"
-        }
+        var result = "N/A"
+
+        books_list_array.forEach({ books_list in
+            
+            let filtered = books_list.results.filter { $0.id == bookId }
+            
+            if (filtered.isEmpty)
+            {
+                result = "N/A"
+            } else {
+                result = filtered.first?.title ?? "N/A"
+            }
+            
+        })
+        
+        return result
     }
 }
